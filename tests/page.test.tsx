c46e3548c0a4049af06f5d6c8e80a0166d680ea6
Matt F <114502585+AuhUsername@users.noreply.github.com>
2024-05-29 17:38:42 +0100

    [mf822] page.test.tsx: [Fix rendering of async component Page() in testing using await]

--- tests/page.test.tsx
+++ tests/page.test.tsx
@@ -4,12 +4,12 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import Page from '../src/app/page';
 
 describe('Page', () => {
-  it('renders a heading', () => {
-    // render(<Page />);
+  it('renders a heading', async () => {
+    render(await Page());
  
-    // const heading = screen.getByRole('heading', { level: 1 })
+    const heading = screen.getByRole('heading', { level: 1 })
  
-    // expect(heading).toBeInTheDocument()
+    expect(heading).toBeInTheDocument()
     // expect(heading).toHaveTextContent('Hello, World!')
   })
 })
