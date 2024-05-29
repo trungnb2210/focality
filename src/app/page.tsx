4cbb25c7cccd683adac8055819f78fc1efb5c8f5
Jack Nguyen <jn1122@ic.ac.uk>
2024-05-29 17:30:56 +0100

    [jn1122] /prisma: Modify build script to fix database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,17 @@ import prisma from "../../lib/primsa";
 
 export default async function Home() {
   // Fetch data from Prisma
-  const data = await prisma.store.findMany();
+  const stores = await prisma.store.findMany();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Hello, World to me!</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h1>Focality</h1>
+      <pre>{JSON.stringify(stores, null, 2)}</pre>
+      <div>
+        {stores.map((store, index) => (
+          <p key={index}>{store.name} : {store.sortcode}</p>
+        ))}
+      </div>
     </main>
   );
 }
