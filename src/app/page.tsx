850edc5aaddcb52b59467eb595122daa3d1eaf08
Jack Nguyen <jn1122@ic.ac.uk>
2024-05-29 17:01:59 +0100

    [jn1122] /root: Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
-// import prisma from "../../lib/primsa";
-
-// export default async function Home() {
-//   // Fetch data from Prisma
-//   const data = await prisma.store.findMany();
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-//       <h1>Hello, World to me!</h1>
-//       <pre>{JSON.stringify(data, null, 2)}</pre>
-//     </main>
-//   );
-// }
+import prisma from "../../lib/primsa";
 
 export default async function Home() {
+  // Fetch data from Prisma
+  const data = await prisma.store.findMany();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Hello, World to Linh!</h1>
+      <h1>Hello, World to me!</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </main>
   );
 }
