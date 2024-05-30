5633aabf64fb5b2708a2c5d6cddf35497cb6d792
Matthew Ferriman <matthew.ferriman22@imperial.ac.uk>
2024-05-30 21:48:21 +0100

    [mf822] list-of-store-page.tsx: Add display of stores where items match ingredients in query

--- src/pages/list-of-store-page.tsx
+++ src/pages/list-of-store-page.tsx
@@ -1,23 +1,68 @@
 "use client";
 
-import React, { useState } from 'react';
-import NavBar from '../components/NavBar';
-import FindStoreButton from '../components/FindStoreButton';
-import IngredientItems from '../components/IngredientItems';
-import { FaPlus } from 'react-icons/fa';
-import Link from 'next/link';
-import SearchIngredientPage from './search';
+import { GetServerSideProps } from 'next';
+import React from 'react';
+import prisma from '../../lib/primsa';
 
-interface IngredientsPageProps {
-    ingredients: string[];
-  }
+interface Store {
+  sid: string;
+  name: string | null;
+  sortcode: string;
+}
 
-const ListOfStorePage: React.FC<IngredientsPageProps> = ({  ingredients }) => {
+interface ListOfStorePageProps {
+  stores: Store[];
+  ingredients: string[];
+}
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { ingredients } = context.query;
+
+  // Ensure ingredients are passed as an array of strings
+  const ingredientsArray = Array.isArray(ingredients) ? ingredients : [ingredients];
+
+
+  const stores = await prisma.store.findMany({
+    where: {
+      items: {
+        some: {
+          name: {
+            in: ingredientsArray as string[],
+          },
+        },
+      },
+    },
+    select: {
+      sid: true,
+      name: true,
+      sortcode: true,
+    },
+  });
+
+  return {
+    props: {
+      stores,
+      ingredients: ingredientsArray,
+    },
+  };
+}
+
+const ListOfStorePage: React.FC<ListOfStorePageProps> = ({ stores, ingredients }) => {
   return (
-    <div className="flex flex-col h-[812px] justify-between">
-      
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>Focality</h1>
+      <div>
+        {stores.length > 0 ? (
+          stores.map((store) => (
+            <div key={store.sid} className="store">
+              <p>{store.name} : {store.sortcode}</p>
+            </div>
+          ))
+        ) : (
+          <p>No stores found for the given ingredients.</p>
+        )}
+      </div>
+    </main>
   );
 };
 
